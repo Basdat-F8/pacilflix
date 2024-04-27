b0a3c34d10001d000ee0092f88668e4a833973cb
carissadzr <carissadzr@gmail.com>
2024-04-27 18:49:05 +0700

    daftar favorit dan unduhan

--- main/models.py
+++ main/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+import uuid
+
+'''
+class daftar_favorit(models.Model):
+    judul = models.CharField(max_length=50) 
+    username = models.CharField(max_length=50) # fk pengguna.username
+    timestamp = models.DateTimeField()
+
+class daftar_unduhan(models.Model):
+    id_tayangan = models.UUIDField(default=uuid.uuid4, editable=False, unique=True) # fk tayangan.id
+    username = models.CharField(max_length=50) # fk pengguna.username
+    timestamp = models.DateTimeField()
+'''
--- main/views.py
+++ main/views.py
@@ -40,3 +40,23 @@ def login_user(request ):
 def logout_user(request):
     logout(request)
     return redirect('main:login')
+
+def film_favorit(request):
+    # Data favorit film statis
+    data_favorit = [ #contoh
+        {"judul": "Film A", "waktu_ditambahkan": "2024-04-27 14:30:00"},
+        {"judul": "Film B", "waktu_ditambahkan": "2024-04-26 12:45:00"},
+        {"judul": "Film C", "waktu_ditambahkan": "2024-04-25 10:15:00"},
+    ]
+    # Render template dengan data favorit film
+    return render(request, 'favorit.html', {'data_favorit': data_favorit})
+
+def film_unduhan(request):
+    # Data favorit film statis
+    data_unduhan = [ #contoh
+        {"judul": "Film A", "waktu_ditambahkan": "2024-04-27 14:30:00"},
+        {"judul": "Film B", "waktu_ditambahkan": "2024-04-26 12:45:00"},
+        {"judul": "Film C", "waktu_ditambahkan": "2024-04-25 10:15:00"},
+    ]
+    # Render template dengan data favorit film
+    return render(request, 'unduhan.html', {'data_unduhan': data_unduhan})
